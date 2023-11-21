401f3a6a515bd44d13cc49b48d9d6277cfa48ecb
KARTHICK877 <kmass8754@gmail.com>
2023-11-21 14:17:19 +0530

    first commit

---
+++ web/src/pages/About.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Content from '../components/Content';
+import Gallery from '../components/Gallery';
+const About = () => {
+  // JSX content here
+  return (
+    <div>
+       <Content />
+    <Gallery />
+    </div>
+  );
+};
+
+export default About;
